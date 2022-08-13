da5b35a0fa638801a5218c70cd813766d85a4694
Henry Bach <henrypbach@gmail.com>
2022-08-12 21:01:12 -0400

    next plan

--- pexels-api-text.js
+++ pexels-api-text.js
@@ -1,13 +1,27 @@
+//wrap everything in an async function?
 import {
   createClient
 } from 'pexels';
 
-const client = createClient('563492ad6f917000010000015a0408d31033415caf82c979840d22bc');
-const query = 'Nature';
+const api_key = '563492ad6f917000010000015a0408d31033415caf82c979840d22bc';
+const client = createClient(api_key);
+//const query = 'Nature';
+var image;
 
-client.photos.search({
-  query,
-  per_page: 1
-}).then(photos => {
-  console.log(photos)
-});
+async function images(query) {
+  try {
+    let data = await client.photos.search({
+      query,
+      per_page: 1
+    });
+    let photo = data.photos[0];
+    let src = photo.src;
+    image = "abc";//src.landscape;
+    console.log(image);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+await images("lake");
+console.log(image);
