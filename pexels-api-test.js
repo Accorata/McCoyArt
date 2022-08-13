948e7a0dce72ef916a0191e6fcfcb5b5c098ab8b
Henry Bach <henrypbach@gmail.com>
2022-08-12 20:31:23 -0400

    function to grab images

--- pexels-api-test.js
+++ pexels-api-test.js
@@ -7,20 +7,39 @@
 // // client.photos.search({ query, per_page: 1 })
 
 const api_key = '563492ad6f917000010000015a0408d31033415caf82c979840d22bc';
+//const query = "lake";
 
 import fetch from "node-fetch";
 
-fetch("https://api.pexels.com/v1/search?query=people",{
-  headers: {
-    Authorization: api_key
-  }
-})
-   .then(resp => {
-     return resp.json()
-   })
-   .then(data => {
-     console.log(data)
-   })
-   .catch(resp => {
-     console.log("Fail")
-   })
+async function images(query) {
+    let response = await fetch("https://api.pexels.com/v1/search?query=" + query, {
+        headers: {
+          Authorization: api_key
+        }
+      });
+    let data = await response.json();
+    console.log(data);
+}
+
+images("lake");
+
+// const data = await fetch("https://api.pexels.com/v1/search?query=" + query, {
+//     headers: {
+//       Authorization: api_key
+//     }
+//   })
+//   .then(resp => {
+//     return resp.json()
+//   })
+//   .then(data => {
+//     return data
+//   })
+//   .catch(error => {
+//     console.log("Fail")
+//     return ""
+//   });
+//
+//   // .then(data => {
+//   //   console.log(data)
+//   // })
+// console.log(data);
