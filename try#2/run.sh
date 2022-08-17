8aadb2d53742d34967708329f3fc9a884d7f6244
Henry Bach <henrypbach@gmail.com>
2022-08-17 10:18:22 -0400

    can get image files

--- try#2/run.sh
+++ try#2/run.sh
@@ -5,3 +5,5 @@ node fetch_urls.js
 echo "abc"
 # node grab_urls.js
 # |> echo ""
+
+# curl --url "https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200" -o test.jpeg
