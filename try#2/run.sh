85d993d52b2bb4e645444a151cb49a3059a98ddd
Henry Bach <henrypbach@gmail.com>
2022-08-18 13:34:21 -0400

    Including gitignore + organization

--- try#2/run.sh
+++ try#2/run.sh
@@ -11,47 +11,11 @@ function save_image() {
 
 function save_images() {
   for q in ${queries[@]};
-    do file_name="${q}.jpeg"
+    do file_name="images/${q}.jpeg"
     node fetch_urls.js ${q} | save_image;
   done;
 }
-# file_name="sea.jpeg"
-# node fetch_urls.js sea | save_image
-# file_name="sea.jpeg"
-# node fetch_urls.js sea | save_image
-# file_name="sea.jpeg"
-# node fetch_urls.js sea | save_image
-# file_name="sea.jpeg"
-# node fetch_urls.js sea | save_image
+
 save_images
 
 echo "done"
-#one=(1 2 3 4)
-# function save_images() {
-#
-# }
-#
-# function set_one(){
-#   while read data;
-#   do one=${data};
-#   done;
-# }
-#
-# function split() {
-#   for t in ${one[@]};
-#     do echo $t
-#   done;
-# }
-#
-# node test.js | set_one
-#
-# split ${one}
-
-
-
-
-
-
-# function save_image() {
-#   curl --url "$1" -o test3.jpeg
-# }
