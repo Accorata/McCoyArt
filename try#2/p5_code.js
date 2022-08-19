4401d2e8a7a5e7d01e6de79990b8924c8a560504
Henry Bach <henrypbach@gmail.com>
2022-08-18 23:45:50 -0400

    top mask

--- try#2/p5_code.js
+++ try#2/p5_code.js
@@ -1,11 +1,20 @@
-let sky_img;
+let sky_img, sea_img, land_img, sun_img;
+let remove_top_mask;
 
 function preload() {
   sky_img = loadImage("images/sky.jpeg");
+  sea_img = loadImage("images/sea.jpeg");
+  land_img = loadImage("images/landscape.jpeg");
+  sun_img = loadImage("images/sunset.jpeg");
+  remove_top_mask = loadImage("masks/remove_top.png");
 }
 
 function setup () {
   createCanvas (1200,627);
-  background(0);
   image(sky_img, 0, 0);
+  //background(0);
+
+  sea_img.mask(remove_top_mask);
+  image(sea_img, 0, 0);
+  //image(remove_top_mask);
 }
