8080f37a367a93512b0448f635bf1273b9a75a9e
Henry Bach <henrypbach@gmail.com>
2022-08-31 14:53:51 -0400

    almost works

--- project/circle_mask_generator/circle_mask_generator.js
+++ project/circle_mask_generator/circle_mask_generator.js
@@ -1,16 +1,16 @@
 function setup () {
-	createCanvas(1200, 627);
-  //background(50);
+	createCanvas (1200, 627);
 	let img = createGraphics(width, height);
-  let center = new PVector(random(100,1100), random(100, 327));
+  let radius = 75;
+  let centerX = random(radius, width-radius);
+  let centerY = random(radius, height/2-radius);
   for (let x = 0; x<width; x++) {
     for (let y = 0; y<height; y++) {
-      if (dist(x, y, center.x, center.y) > 100) {
+      if (dist(x, y, centerX, centerY) > radius) {
         img.point(x, y);
       }
-      //println("y");
     }
   }
 	image(img, 0, 0);
-  save(img, "circle_mask.png");
+  //save(img, "circle_mask.png");
 }
--- project/p5_code.js
+++ project/p5_code.js
@@ -7,12 +7,21 @@ function preload() {
   land_img = loadImage("images/landscape.jpeg");
   sun_img = loadImage("images/sunset.jpeg");
   remove_top_mask = loadImage("masks/remove_top.png");
-  let mask_number = int(random()*10);
-  land_mask = loadImage("masks/generated/random_mask_"+mask_number+".png");
+  let mask_number = int(random() * 10);
+  land_mask = loadImage("masks/generated/random_mask_" + mask_number + ".png");
 }
 
-function setup () {
-  createCanvas (1200,627);
+function setup() {
+  createCanvas(1200, 627);
+
+  let circle_mask = createGraphics(width, height);
+  let radius = 75;
+  let centerX = random(radius, width - radius);
+  let centerY = random(radius, height / 2 - radius);
+
+  fill(0);
+  circle_mask.ellipse(centerX, centerY, radius, radius);
+
   image(sky_img, 0, 0);
 
   sea_img.mask(remove_top_mask);
@@ -20,4 +29,9 @@ function setup () {
 
   land_img.mask(land_mask);
   image(land_img, 0, 0);
+
+  let masked;
+  (masked = sun_img.get()).mask(circle_mask);
+  //sun_img.mask(circle_mask);
+  image(sun_image, 0, 0);
 }
