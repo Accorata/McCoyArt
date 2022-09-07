4aafdf2e9505f4f1c6879a445d25a9ed71e333ad
Henry Bach <henrypbach@gmail.com>
2022-09-07 11:49:33 -0400

    generates land mask when ran

--- project/p5_code.js
+++ project/p5_code.js
@@ -1,5 +1,5 @@
 let sky_img, sea_img, land_img, sun_img;
-let remove_top_mask, land_mask;
+let remove_top_mask;//, land_mask;
 
 function preload() {
   sky_img = loadImage("images/sky.jpeg");
@@ -7,21 +7,38 @@ function preload() {
   land_img = loadImage("images/landscape.jpeg");
   sun_img = loadImage("images/sunset.jpeg");
   remove_top_mask = loadImage("masks/remove_top.png");
-  let mask_number = int(random() * 10);
-  land_mask = loadImage("masks/generated/random_mask_" + mask_number + ".png");
+  // let mask_number = int(random() * 10);
+  // land_mask = loadImage("masks/generated/random_mask_" + mask_number + ".png");
 }
 
 function setup() {
-  createCanvas(1200/3, 627/3);
+  createCanvas(1200/1.5, 627/1.5);
 
+  //Sky
   image(sky_img, 0, 0, width, height);
 
+  //Sea
   sea_img.mask(remove_top_mask);
   image(sea_img, 0, 0, width, height);
 
-  land_img.mask(land_mask);
-  image(land_img, 0, 0, width, height);
+  //Land
+  let land_graphics = createGraphics(width, height);
+  land_graphics.image(land_img, 0, 0, width, height);
 
+  let land_mask = createGraphics(width, height);
+  for (let x = 0; x<width; x++) {
+    land_mask.line(x, height, x, height - noise(x*0.003) * height);
+  }
+
+  let land = land_graphics.get();
+  land.mask(land_mask.get());
+
+  image(land, 0, 0, width, height);
+
+  // land_img.mask(land_mask);
+  // image(land_img, 0, 0, width, height);
+
+  //Sun
   let sun_graphics = createGraphics(width, height);
   sun_graphics.image(sun_img, 0, 0, width, height);
 
