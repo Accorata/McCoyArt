5827b104e1e30e6bd0576fd60002108f7921c632
Henry Bach <henrypbach@gmail.com>
2023-06-22 21:01:04 -0400

    create_grid()

--- 02-Disappearing Image/app/static/disappear.js
+++ 02-Disappearing Image/app/static/disappear.js
@@ -1,5 +1,20 @@
 console.log("tada");
 
-let svg = document.getElementById("image");
+// console.log(svg);
 
-console.log(svg);
+function create_grid (x, y, square_number) {
+  let x_step = x/square_number;
+  let y_step = y/square_number;
+  let grid = [];
+  for (let i = 0; i<square_number; i++) {
+    for (let j = 0; j<square_number; j++) {
+      grid.push([i*x_step, j*y_step]);
+    }
+  }
+  return grid;
+}
+
+let svg = document.getElementById("image");
+let grid = create_grid(svg.width, svg.height, 10);
+// console.log(grid);
+// console.log(grid[3]);
